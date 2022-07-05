083f2eaa83012c825b6f381a3ef28ef9f709f7bc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-05 18:42:07 -0300

    fix: update schema

--- src/models/Character.js
+++ src/models/Character.js
@@ -5,10 +5,6 @@ const CharacterSchema = new mongoose.Schema({
         type: String,
         require: true,
     },
-    description: {
-        type: String,
-        require: true,
-    },
     image: {
         type: String,
         require: true,
