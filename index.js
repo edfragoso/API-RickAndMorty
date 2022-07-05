8cd8e5e577a7629b34e738ca8061191c31720312
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-05 18:20:48 -0300

    chore: add conncetDB

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./src/database/database');
 const port = 3000;
 
 const app = express();
+
+connectDB();
 app.use(cors());
 app.use(express.json());
 
