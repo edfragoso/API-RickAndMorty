47a4b0bad4703b3cadd7d6aa513bec6a5f0de9b1
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-06 15:12:45 -0300

    chore: add findAll

--- src/services/character.services.js
+++ src/services/character.services.js
@@ -1,1 +1,12 @@
 const Character = require('../models/Character');
+
+const findAllCharacters = async () => {
+     return await Character.find();
+};
+
+
+
+module.exports = {
+    findAllCharacters,
+    findByIdCharacters,
+}
