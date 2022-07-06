cbafc944b1f52cb5cbde508ed50b06c72d6e7658
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-06 17:23:27 -0300

    chore: route create

--- src/routes/characters.routes.js
+++ src/routes/characters.routes.js
@@ -3,6 +3,7 @@ const controllerCharacter = require('../controllers/characters.controller');
 
 route.get('/characters', controllerCharacter.findAllCharacters);
 route.get('/characters/find/:id', controllerCharacter.findByIdCharacters);
+route.post('/character/create', controllerCharacter.createCharacter);
 
 
 module.exports = route;
