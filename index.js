eccef4f203cb6664be28e12821e5fc756ce7f055
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-06 14:50:49 -0300

    fix: update route and app.listen

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const routes = require('./src/routes/characters.routes');
 const connectDB = require('./src/database/database');
 const port = 3000;
 
 const app = express();
 
 connectDB();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send();
-});
- 
-app.listen(() => {
+
+
+app.use("/", routes);
+
+app.listen(port,() => {
     console.log(`Servidor Rodando em http://localhost:${port}`)
 });
