71dda07676e5c2ccf5c7abaf4816073b74918453
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-06 16:00:42 -0300

    chore: route getById

--- src/routes/characters.routes.js
+++ src/routes/characters.routes.js
@@ -2,6 +2,7 @@ const route = require('express').Router();
 const controllerCharacter = require('../controllers/characters.controller');
 
 route.get('/characters', controllerCharacter.findAllCharacters);
+route.get('/characters/find/:id', controllerCharacter.findByIdCharacters);
 
 
 module.exports = route;
