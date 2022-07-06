8feebd1246729fc8fb3151877f799210afc28696
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-06 15:10:50 -0300

    chore: add getAll

---
+++ src/controllers/characters.controller.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const charactersService = require('../services/character.services');
+
+const findAllCharacters = async (req, res) => {
+    const allCharacters = await charactersService.findAllCharacters();
+    
+    if (allCharacters.length == 0) {
+        return res.status(404).send({ message: "Não existe nenhum personagem cadastrado" });
+    }
+    res.send('allCharacters');
+}
+
+
+module.exports = {
+    findAllCharacters,
+}
