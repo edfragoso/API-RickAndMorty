1dbf62f0868f320dde1761e550929ac913a932e3
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-07 17:13:24 -0300

    feat: edit teste commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # API-RickAndMorty
 Project03-M3-RickAndMorty
+<p>Testando commit</p>
--- src/routes/characters.routes.js
+++ src/routes/characters.routes.js
@@ -1,6 +1,11 @@
 const route = require('express').Router();
 const controllerCharacter = require('../controllers/characters.controller');
 const { validId, validObjectBody } = require('../middleware/characters.middleware');
+/* const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json'); */
+/* 
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument)); */
 
 route.get('/characters', controllerCharacter.findAllCharacters);
 route.get('/characters/find/:id', validId, controllerCharacter.findByIdCharacters);
